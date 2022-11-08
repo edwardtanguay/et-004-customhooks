725138b2b8a8cf573831c3fe26e1efacc3eec8df
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-08 10:21:14 +0100

    show ads button works

--- src/components/PageUseToggle.tsx
+++ src/components/PageUseToggle.tsx
@@ -1,7 +1,13 @@
+import { useState } from 'react';
+
 export const PageUseToggle = () => {
+	const [showAds, setShowAds] = useState(false);
+
 	return (
 		<>
 			<p>The useToggle page.</p>
+			<hr/>
+			<button onClick={() => setShowAds(!showAds)}>Advertisements</button> {showAds ? 'Yes, show me ads' : 'No, do not show me ads.'}
 		</>
 	);
 };
