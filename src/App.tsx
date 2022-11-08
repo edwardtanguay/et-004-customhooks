8d22878e6988de59dd4adb6a341af9253d016a1e
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-08 09:58:53 +0100

    navlinks implemented

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,18 @@ import './App.scss';
 import { PageUseFetch } from './components/PageUseFetch';
 import { PageUseToggle } from './components/PageUseToggle';
 import { PageWelcome } from './components/PageWelcome';
+import { NavLink, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Custom Hooks Site</h1>
+
+			<nav>
+				<NavLink to="/welcome">Welcome</NavLink>
+				<NavLink to="/useToggle">useToggle</NavLink>
+				<NavLink to="/useFetch">useFetch</NavLink>
+			</nav>
 
 			<PageWelcome />
 			<PageUseToggle />
