d777b9821dda332ed37f642f47e9f17f9dabbbc7
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-08 10:55:17 +0100

    send mail is in

--- src/components/PageUseToggle.tsx
+++ src/components/PageUseToggle.tsx
@@ -2,15 +2,28 @@ import { useState } from 'react';
 
 export const PageUseToggle = () => {
 	const [showAds, setShowAds] = useState(false);
+	const [sendMail, setSendMail] = useState(false);
 
 	return (
-		 <div className="pageUseToggle">
+		<div className="pageUseToggle">
 			<p>The useToggle page.</p>
-			<hr/>
-			<button onClick={() => setShowAds(!showAds)}>Advertisements</button>{' '}
-			<span className={showAds ? 'positive' : 'negative'}>
-			{showAds ? 'Yes, show me ads' : 'No, do not show me ads.'}
-			</span>	
-		 </div>
+			<hr />
+			<div className="toggleArea">
+				<button onClick={() => setShowAds(!showAds)}>
+					Advertisements
+				</button>{' '}
+				<span className={showAds ? 'positive' : 'negative'}>
+					{showAds ? 'Yes, show me ads.' : 'No, do not show me ads.'}
+				</span>
+			</div>
+			<div className="toggleArea">
+				<button onClick={() => setSendMail(!sendMail)}>Mail</button>{' '}
+				<span className={sendMail ? 'positive' : 'negative'}>
+					{sendMail
+						? 'Yes, send me mail.'
+						: 'No, do not send me mail.'}
+				</span>
+			</div>
+		</div>
 	);
 };
